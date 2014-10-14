0ca53f34aea65e82b59c172ff9885c8069c64da1
Esben Sonne <esbensonne@gmail.com>
2014-10-14 19:29:50 +0200

    Rewrote to reflect change in beetle branch.

--- beetle_image_compressor/__init__.py
+++ beetle_image_compressor/__init__.py
@@ -1,12 +1,13 @@
 from PIL import Image
-# from Pillow import Image
 from io import BytesIO
 
-def compress(path):
-    img = Image.open(path)
-    buffer_file = BytesIO()
+
+def compress(content, suggested_path):
+    buffer_file = BytesIO(content)
+    img = Image.open(buffer_file)
     img.save(buffer_file, 'JPEG', quality=60)
-    return 'jpeg', buffer_file.getvalue()
+    return suggested_path, buffer_file.getvalue()
+
 
 def register(context, plugin_config):
     jpeg_extensions = ['jpeg', 'jpg']
